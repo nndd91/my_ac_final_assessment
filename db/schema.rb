8066df878aede3b40dc2bbcf36d957b7af319bfd
nndd91 <nndd91@hotmail.com>
2017-07-06 10:57:57 +0800

    Generate Notes model and controller and updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706022339) do
+ActiveRecord::Schema.define(version: 20170706024842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 20170706022339) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
